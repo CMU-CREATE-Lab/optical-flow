a33c434e6996515d65bb2b737c77927c2a532d02
SeanPrendi <sean.prendi@gmail.com>
2019-02-18 15:09:48 -0500

    Updated OpticalFlowTest to use requests library and changed it to test negative labels

--- OpticalFlowTest.py
+++ OpticalFlowTest.py
@@ -1,9 +1,6 @@
 from OpticalFlow import *
+import requests
 import json
-
-read = open("optical-flow/Positive Labels.json").read()
-
-positive_data = json.loads(read)
 
 
 def main():
@@ -12,23 +9,28 @@ def main():
     test = OpticalFlow()
     pixel_values_array = [70, 80, 90, 100, 110, 120, 130]
     frame_values_array = [0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
-    for pixel in pixel_values_array:
-        for frame in frame_values_array:
-            filtered = 0
-            total = 0
-            for i in positive_data["data"]:
-                test.step(i["url_root"] + i["url_part"])
-                if not test.threshold(pixel, frame, 0.3):
-                    filtered += 1
-                total += 1
-            frame_thresh_values[frame] = (
-            filtered, total, filtered / total * 100)
-            print(frame, filtered / total * 100)
-            if filtered / total * 100 > 10:
-                break
-        pixel_thresh_values[pixel] = frame_thresh_values
-        frame_thresh_values = {}
-    print(pixel_thresh_values)
+    for page in ['1', '2', '3', '4']:
+        url = 'http://api.smoke.createlab.org/api/v1/get_pos_labels_by_researcher?&pageSize=1000'
+
+        params = {'pageSize': '1000', 'pageNumber':page}
+
+        response = requests.get(url=url, params=params)
+        videos = json.loads(response.text)
+        for pixel in pixel_values_array:
+            for frame in frame_values_array:
+                filtered = 0
+                total = 0
+                for i in videos["data"]:
+                    test.step(i["url_root"] + i["url_part"])
+                    if not test.threshold(pixel, frame, 0.3):
+                        filtered += 1
+                    total += 1
+                    print("  " + str(i['id']))
+                frame_thresh_values[frame] = (filtered, total, filtered / total * 100)
+                print(frame, filtered / total * 100)
+            pixel_thresh_values[pixel] = frame_thresh_values
+            frame_thresh_values = {}
+        print(pixel_thresh_values)
 
 
 print(main())
