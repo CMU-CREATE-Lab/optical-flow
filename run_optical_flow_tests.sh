a67f207e5dfe97d262fde76607f23372e1e1efaf
SeanPrendi <sean.prendi@gmail.com>
2019-05-21 11:27:27 -0400

    Updated to account for running multiple tests concurrently

--- run_optical_flow_tests.sh
+++ run_optical_flow_tests.sh
@@ -1,7 +1,14 @@
-sudo screen -x test_optical_flow
+# Delete existing screen
+for session in $(sudo screen -ls | grep -o '[0-9]\{5\}.test_optical_flow')
+do
+sudo screen -S "${session}" -X quit
+done
+
+# Delete the log
 sudo rm screenlog.0
 
 # For python in conda env
-sudo screen -dmSL "test_optical_flow" bash -c "export PATH='/opt/miniconda3/bin:$PATH'; . '/opt/miniconda3/etc/profile.d/conda.sh'; conda activate optical-flow; python OpticalFlowTest.py"
+sudo screen -dmSL "test_optical_flow" bash -c "export PATH='/opt/miniconda3/bin:$PATH'; . '/opt/miniconda3/etc/profile.d/conda.sh'; conda activate video-labeling-tool; python OpticalFlowTest.py"
 
+# List screens
 sudo screen -ls
