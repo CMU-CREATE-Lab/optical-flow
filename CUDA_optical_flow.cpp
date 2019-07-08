88f523ec6f066dc00ec78ea2a6bad06534746ee3
SeanPrendi <sean.prendi@gmail.com>
2019-07-08 14:29:42 -0400

    Updated to properly account for memory handling.

--- CUDA_optical_flow.cpp
+++ CUDA_optical_flow.cpp
@@ -2,32 +2,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 using namespace cv;
 using namespace cv::cuda;
 
-extern "C" void calc_cuda_flow (int rows, int cols, uint8_t *previous, uint8_t *current, uint8_t *flow){
-	Ptr<cv::cuda::OpticalFlowDual_TVL1> opt = cv::cuda::OpticalFlowDual_TVL1::create();
+extern "C" void calc_cuda_flow (int rows, int cols, float *previous, float *current, float *flow){
+  Ptr<cv::cuda::OpticalFlowDual_TVL1> opt = cv::cuda::OpticalFlowDual_TVL1::create();
 	
-	cv::Mat previous_gray(rows, cols, CV_8UC1, (void*) previous);
-	cv::cuda::GpuMat gpu_prev_gray;
-	gpu_prev_gray.upload(previous_gray);
-	
-	cv::Mat current_gray(rows, cols, CV_8UC1, (void*) current);
-	cv::cuda::GpuMat gpu_curr_gray;
-	gpu_curr_gray.upload(current_gray);
+  cv::Mat cpu_flow;
+  cv::Mat previous_gray(rows, cols, CV_8UC1, (void*) previous);
+  //cv::cuda::GpuMat gpu_prev_gray;
+  //gpu_prev_gray.upload(previous_gray);
 
-	cv::Mat f(rows, cols, CV_8UC2, (void*) flow);
-	cv::cuda::GpuMat gpu_flow;
-	gpu_flow.upload(f);
+  cv::Mat current_gray(rows, cols, CV_8UC1, (void*) current);
+  //cv::cuda::GpuMat gpu_curr_gray;
+  //gpu_curr_gray.upload(current_gray);
 
-	opt->calc(gpu_prev_gray, gpu_curr_gray, gpu_flow);
-	cv::Mat cpu_flow(gpu_flow);
-	uint8_t *data = cpu_flow.data;
-	for(uint16_t i; i < rows*cols*2; i++){
-		flow[i] = data[i];
-	}
+  cv::Mat f(rows, cols, CV_8UC2, (void*) flow);
+  //cv::cuda::GpuMat gpu_flow;
+  //gpu_flow.upload(f);
+
+  cv::cuda::GpuMat gpu_prev_gray;
+  gpu_prev_gray.upload(previous_gray);
+  cv::cuda::GpuMat gpu_curr_gray;
+  gpu_curr_gray.upload(current_gray);
+  cv::cuda::GpuMat gpu_flow;
+  gpu_flow.upload(f);
+
+  opt->calc(gpu_prev_gray, gpu_curr_gray, gpu_flow);
+  //cv::Mat cpu_flow(gpu_flow);
+  gpu_flow.download(cpu_flow);
+
+  //uint8_t *data = cpu_flow.data;
+
+  for(size_t i = 0; i < rows*cols*2; i++){
+    float x = cpu_flow.at<float>(i);
+    flow[i] = x;
+  }
 }
+/*
+  size_t index = 0;
+  for(int i; i < rows; i++){
+    for(int j; j < cols; j++){
+      for(int k; k < 2; k++){
+        float x = cpu_flow.at<float>(i,j,k);
+        flow[index] = x;
+        index += 1;
+      }
+    }
+  }
+}
+*/
 
 int main(int argc, char* argv[]){
-	return 0;
+  return 0;
 }
